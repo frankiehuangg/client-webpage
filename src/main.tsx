756334eee3f23997368c63e93ef639e623ccddab
frankiehuangg <13521092@std.stei.itb.ac.id>
2023-10-27 12:49:59 +0700

    feat: add default page layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Routes from './routes/routes.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Col, Container, Row } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Routes from './routes/routes.tsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
+  <Container fluid >
+    <Row>
+      <Col xs={12} md={3} className='sidebar'>
+      </Col>
+      <Col xs={12} md={9} className='mainbar'>
+        <React.StrictMode>
+          <Routes />
+        </React.StrictMode>
+      </Col>
+    </Row>
+  </Container>,
 )
